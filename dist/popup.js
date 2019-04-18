1eafbbc88172d43fecb574634eb958cf08be5084
vitalii_strelnikov <vitalii_s@codesushi.co>
2019-04-18 14:46:59 +0300

    extension v.0.0.1

--- dist/popup.js
+++ dist/popup.js
@@ -2,34 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('test');
     checkPageButton.addEventListener('click', function() {
         chrome.tabs.executeScript({
-            code: '(function(console) {\n' +
+            code: '(function (consosle) {\n' +
+                '    console.saveToZapier = async function(name, linksArr) {\n' +
+                '        const zapierHookUrl = \'https://hooks.zapier.com/hooks/catch/80145/75wa0i/\';\n' +
                 '\n' +
-                '    console.save = function(data, filename) {\n' +
+                '        let data = {eventName: name, links: linksArr};\n' +
                 '\n' +
-                '        if (!data) {\n' +
-                '            console.error(\'Console.save: No data\');\n' +
-                '            return;\n' +
-                '        }\n' +
+                '        let response = await fetch(zapierHookUrl, {\n' +
+                '            method: "POST",\n' +
+                '            body: JSON.stringify(data),\n' +
+                '        });\n' +
                 '\n' +
-                '        if (!filename) filename = \'console.json\';\n' +
+                '        let responseData = await response.json();\n' +
+                '        console.log(responseData);\n' +
+                '        \n' +
+                '        return responseData;\n' +
                 '\n' +
-                '        if (typeof data === \'object\') {\n' +
-                '            data = JSON.stringify(data, undefined, 4);\n' +
-                '        }\n' +
+                '    } \n' +
                 '\n' +
-                '        var blob = new Blob([data], { type: \'text/json\' }),\n' +
-                '            e = document.createEvent(\'MouseEvents\'),\n' +
-                '            a = document.createElement(\'a\');\n' +
-                '\n' +
-                '        a.download = filename;\n' +
-                '        a.href = window.URL.createObjectURL(blob);\n' +
-                '        a.dataset.downloadurl = [\'text/json\', a.download, a.href].join(\':\');\n' +
-                '        e.initMouseEvent(\'click\', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);\n' +
-                '        a.dispatchEvent(e);\n' +
-                '    };\n' +
                 '})(console);' +
-                'links = Array.prototype.slice.call(document.querySelectorAll(\'div.uiProfileBlockContent div._6a div.fsl a\')).map(item => item.getAttribute(\'href\'));\n' +
-                'console.save(links);'
+                'links = Array.prototype.slice.call(document.querySelectorAll(\'div.ellipsis a\')).map(item => item.getAttribute(\'href\'));\n' +
+                'console.saveToZapier(links);'
         });
 
     }, false);
